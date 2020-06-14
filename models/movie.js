3d993abb20ed7f3e7ca70eb22d32e596d1d97e6f
tregdor <49729985+tregdor@users.noreply.github.com>
2020-06-14 18:36:32 +0300

    added schema

---
+++ models/movie.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+  title: String,
+  tagline: String,
+  vote_average: Number,
+  vote_count: Number,
+  release_date: String,
+  poster_path: String,
+  overview: String,
+  budget: Number,
+  revenue: Number,
+  genres: [String],
+  runtime: Number,
+});
+
+module.exports = mongoose.model('movie', movieSchema);
---
+++ schema/schema.js
@@ -0,0 +1,47 @@
+const graphql = require('graphql');
+
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLFloat} = graphql;
+
+const Movies = require('../models/movie');
+
+const MovieType = new GraphQLObjectType({
+  name: 'Movie',
+  fields: () => ({
+    id: {type: GraphQLID},
+    title: {type: GraphQLString},
+    tagline: {type: GraphQLString},
+    release_date: {type: GraphQLString},
+    poster_path: {type: GraphQLString},
+    overview: {type: GraphQLString},
+    genres: {type: new GraphQLList(GraphQLString)},
+    vote_average: {type: GraphQLFloat},
+    vote_count: {type: GraphQLInt},
+    budget: {type: GraphQLInt},
+    revenue: {type: GraphQLInt},
+    runtime: {type: GraphQLInt},
+  }),
+});
+
+const Query = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    movie: {
+      type: MovieType,
+      args: {id: {type: GraphQLID}},
+      resolve(parent, {id}) {
+        return Movies.findById(id);
+      },
+    },
+    movies: {
+      type: new GraphQLList(MovieType),
+      args: {filter: {type: new GraphQLNonNull(GraphQLString)}},
+      resolve(parent, {filter}){
+        return Movies.find({},null, {limit: 10, sort: {[filter]: 'desc'}});
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: Query,
+});
